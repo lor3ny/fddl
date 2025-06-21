f489642a9c7f6ca010266348546097e08de90224
lor3ny <lorenzo.piarulli3000@gmail.com>
2025-06-21 20:42:12 +0200

    MPI Data parallel with CUDA bindings it's working!!!! Eddaje

--- autoencoder_mpi/train_mpi.py
+++ autoencoder_mpi/train_mpi.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Subset
 from torch.utils.data.distributed import DistributedSampler
 from torchvision import datasets, transforms
 
@@ -10,82 +10,144 @@ import torch.distributed as dist
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from mpi4py import MPI
+import numpy as np
 import os
 import argparse
+import time
+import sys
 
 from autoencoder import Autoencoder
 #from mnist_loader import MNISTLoader
 
 
-# class MyTrainDataset(Dataset):
-#     def __init__(self, size):
-#         self.size = size
-#         self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
+class Trainer:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        train_data: DataLoader,
+        test_data: DataLoader,
+        optimizer: torch.optim.Optimizer,
+        criterion: torch.nn.Module,
+        save_every: int,
+        rank: int,
+        gpu_rank: int,
+        size: int,
+        comm: MPI.Comm
+    ) -> None:
+        self.train_data = train_data
+        self.test_data = test_data
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.save_every = save_every
+        self.model = model
+        self.gpu_rank = gpu_rank
+        self.rank = rank
+        self.size = size
+        self.comm = comm
+        self.device="cuda" #  #  Expected one of cpu, cuda, ipu, xpu, mkldnn, opengl, opencl, ideep, hip, ve, fpga, maia, xla, lazy, vulkan, mps, meta, hpu,
 
-#     def __len__(self):
-#         return self.size
-    
-#     def __getitem__(self, index):
-#         return self.data[index]
 
-# class Trainer:
-#     def __init__(
-#         self,
-#         model: torch.nn.Module,
-#         data: MNISTLoader,
-#         optimizer: torch.optim.Optimizer,
-#         criterion: torch.nn.Module,
-#         save_every: int,
-#         rank: int,
-#         gpu_rank: int,
-#         size: int,
-#         comm: MPI.Comm
-#     ) -> None:
-#         self.data = data
-#         self.optimizer = optimizer
-#         self.criterion = criterion
-#         self.save_every = save_every
-#         self.model = model
-#         self.gpu_rank = gpu_rank
-#         self.rank = rank
+    def _save_checkpoint(self, epoch):
+        ckp = self.model.state_dict()
+        PATH = "checkpoint.pt"
+        torch.save(ckp, PATH)
+        print(f"Epoch {epoch} | Training checkpoint saved at {PATH}")
 
-#     def _run_batch(self, inputs):
-#         outputs = self.model(inputs)
-#         loss = self.criterion(inputs, outputs)
+
+    # I don't know if it is correct on an autoencoder
+
+    '''
+    def _evaluate (self) -> None:
         
-#         self.optimizer.zero_grad()
-#         loss.backward()
-#         self.optimizer.step()
+        self.model.eval()
+        correct = 0
+        total = 0
 
-#     def _run_epoch(self, epoch):
-#         print(f"[GPU{self.global_rank}] Epoch {epoch} | Steps: {len(self.data)}")
-#         for images, _ in self.data:
-#             inputs = images.view(-1, 28*28)
-#             inputs = inputs.to(self.local_rank)
-#             self._run_batch(inputs)
+        with torch.no_grad():
+            for data, target in self.test_data:
+                data, target = data.to(self.device), target.to(self.device)
 
-#     def _save_checkpoint(self, epoch):
-#         ckp = self.model.state_dict()
-#         PATH = "checkpoint.pt"
-#         torch.save(ckp, PATH)
-#         print(f"Epoch {epoch} | Training checkpoint saved at {PATH}")
+                output = self.model(data)
+                pred = output.argmax(
+                    dim=1, keepdim=True
+                )  # Get the index of the max log-probability
+                correct += pred.eq(target.view_as(pred)).sum().item()
+                total += target.size(0)
 
-#     def train(self, max_epochs: int):
+        accuracy = 100 * correct / total
+        
+        return accuracy
+    '''
+        
+    def _synchronize_gradients(self) -> None:
 
-#         for epoch in range(max_epochs):
-#             self._run_epoch(epoch)
-#             '''
-#             if self.gpu_id == 0 and epoch % self.save_every == 0:
-#                 self._save_checkpoint(epoch)
-#             '''
+        for param in self.model.parameters():
+            if param.grad is None:
+                continue
+            # Gather gradients from all ranks
+            grad = param.grad.data.cpu().numpy()
+            avg_grad = np.zeros_like(grad)
+            try:
+                self.comm.Allreduce(grad, avg_grad, op=MPI.SUM)
+                avg_grad /= self.size  # Average the gradients
+                param.grad.data = torch.tensor(avg_grad, device=self.device)
+
+            except Exception as e:
+                raise RuntimeError(f"Error synchronizing gradients: {e}.", flush=True)
+
+    def train(self, max_epochs: int):
+
+        start_time = MPI.Wtime()
+        for epoch in range(max_epochs):
+            print(f"[RANK {self.rank} GPU {self.gpu_rank}] Epoch {epoch} | Steps: {len(self.test_data)}")
+            e_start_time = MPI.Wtime()
+            total_loss = 0.0
+
+            #BATCH
+            for images, _ in self.train_data:
+                inputs = images.view(-1, 28*28)
+                inputs = inputs.to(self.gpu_rank)
+                outputs = self.model(inputs)
+                loss = self.criterion(inputs, outputs)
+                
+                self.optimizer.zero_grad()
+                loss.backward()
+
+                self._synchronize_gradients() 
+
+                total_loss += loss.item()
+
+                self.optimizer.step()
+            
+            '''
+            if self.gpu_id == 0 and epoch % self.save_every == 0:
+                self._save_checkpoint(epoch)
+            '''
+
+            # needs averaging
+            epoch_duration = MPI.Wtime() - e_start_time
+
+            avg_loss = total_loss / len(self.train_data)
+            all_avg_loss = (self.comm.allreduce(avg_loss, op=MPI.SUM) / self.size)
+
+            #test_accuracy = self._evaluate()
+
+            self.comm.Barrier()
+            print(f"{self.rank:^5} | {epoch:^7} | {avg_loss:^10.4f} | {all_avg_loss:^11.4f} | {epoch_duration:^10.2f}", flush=True)
+            
+        local_training_time = MPI.Wtime() - start_time
+        max_training_time = self.comm.allreduce(local_training_time, op=MPI.MAX)
+        print(f"Final execution time{max_training_time}") if self.rank == 0 else None
 
 
 # NON SONO CONVINTO VADA BENE, BISOGNA TESTARLO E CORREGGERLO
 
+
 def load_distribute_data(
         rank: int, 
         size: int, 
-        batch_size: int
+        batch_size: int,
+        comm: MPI.Comm
     ) -> tuple[DataLoader, DataLoader]:
 
 
@@ -95,7 +157,7 @@ def load_distribute_data(
             datasets.MNIST(root='./data', train=True, download=False, transform=transform)
             datasets.MNIST(root='./data', train=False, download=True, transform=transform)
             print(f"[RANK: {rank}] DONE.")
-    dist.barrier()
+    comm.barrier()
 
     transform = transforms.ToTensor()
     train_dataset = datasets.MNIST(root='./data', train=True, download=False, transform=transform)
@@ -103,6 +165,9 @@ def load_distribute_data(
 
     # Split dataset into subsets for each rank
     total_samples = len(train_dataset)
+    if rank == 0:
+        print(f"[RANK {rank}] This dataset has {total_samples} samples", flush=True)
+
     sample_per_rank = total_samples // size
     remainder = total_samples % size
     indices = list(range(total_samples))
@@ -113,11 +178,11 @@ def load_distribute_data(
     local_samples = sample_per_rank + extra
     end_index = start_index + local_samples
     local_indices = indices[start_index:end_index]
-    print(f"[RANK: {rank}] I have {local_samples} samples. indices {start_index}:{end_index}")
+    print(f"[RANK {rank}] I have {local_samples} samples. From {start_index} to {end_index}", flush=True)
 
     # Create DataLoader for local dataset
     # Bisogna passare local_indices
-    local_dataset = set(train_dataset, local_indices)
+    local_dataset = Subset(train_dataset, local_indices)
     train_loader = DataLoader(
         local_dataset, batch_size=batch_size, shuffle=True
     )
@@ -141,41 +206,53 @@ def main(
     torch.cuda.set_device(gpu_rank)
 
     if rank == 0:
-        print("x --- LIBRARY CHECK --- x")
-        print(f"PyTorch version: {torch.__version__}")
-        print(f"PyTorch CUDA version: {torch.version.cuda}")
-        print(f"PyTorch CUDNN version: {torch.backends.cudnn.version()}")
-        print(f"GPUs per node {torch.cuda.device_count()}")
-        print(f"Training with GPUs :)") if torch.cuda.is_available() else print("Training with CPUs")
-        print("x --------------------- x")
+        print("+ ---------- TORCH LIBRARY CHECK ---------- +")
+        print(f"PyTorch version: {torch.__version__}", flush=True)
+        print(f"PyTorch CUDA version: {torch.version.cuda}", flush=True)
+        print(f"PyTorch CUDNN version: {torch.backends.cudnn.version()}", flush=True)
+        print(f"GPUs per node {torch.cuda.device_count()}", flush=True)
+        print(f"Training with GPUs :)") if torch.cuda.is_available() else print("Training with CPUs", flush=True)
+        print("+ ----------------------------------------- +")
     
-    comm.barrier()
-    print(f"[Rank {rank}] LOCAL_RANK={gpu_rank} on CUDA device {torch.cuda.current_device()} hostname={os.uname()[1]}")
-    comm.barrier()
+    comm.Barrier()
+    print(f"[Rank {rank}] GPU_RANK={gpu_rank} on CUDA device {torch.cuda.current_device()} hostname={os.uname()[1]}", flush=True)
 
     # DATA MUST BE DIVIDED MANUALLY
-    # train_loader, test_loadr = load_distribute_data(rank=rank, size=size)
+    comm.Barrier()
+    train_loader, test_loader = load_distribute_data(rank=rank, size=size, batch_size=batch_size, comm=comm)
 
     # # Verificare che questa funzione tiri fuori roba seria
 
     # # MODEL INIT
-    # model = Autoencoder(28*28, 32)
-    # criterion = nn.MSELoss()
-    # optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    model = Autoencoder(28*28, 32)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-    # model = model.to(gpu_rank)
-    # model = DDP(model, device_ids=[gpu_rank])
+    model = model.to(gpu_rank)
+    # not needed
+    #model = DDP(model, device_ids=[gpu_rank])
 
-    # # MODEL TRAINING
-    # print(f"[RANK: {rank}] Trainer is running...") if rank == 0 else None
+    # #MODEL TRAINING
+    print(f"[RANK {rank}] Trainer is running...", flush=True) if rank == 0 else None
 
-    # trainer = Trainer(model, train_loader, optimizer, criterion, save_every, rank, size, comm)
-    # trainer.train(epochs)
-    # comm.barrier()
+    trainer = Trainer(
+        model=model,
+        train_data=train_loader,
+        test_data=test_loader,
+        optimizer=optimizer,
+        criterion=criterion,
+        save_every=save_every,
+        rank=rank,
+        gpu_rank=gpu_rank,
+        size=size,
+        comm=comm
+    )
+    trainer.train(epochs)
+    comm.Barrier()
 
     # # Il training deve gestire l'aggregazione del gradiente con la allreduce, rivederlo
 
-    # print(f"[RANK: {rank}] Training done.") if rank == 0 else None
+    print(f"[RANK {rank}] Training done.", flush=True) if rank == 0 else None
 
     # if(rank == 0):
     #     torch.save(model.module.state_dict(), "autoencoder_ddp.pth")
@@ -184,7 +261,7 @@ def main(
 
 if __name__ == "__main__":
 
-    epochs = 1
+    epochs = 5
     batch_size = 64
     save_every = 1
     latent_linear_size = 32
