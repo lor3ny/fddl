3ff195912c5bb7c60e7e3a559631601f7fe837e2
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2025-06-29 11:34:31 +0200

    Update inference.py

--- autoencoder_mpi_pipelining/inference.py
+++ autoencoder_mpi_pipelining/inference.py
@@ -45,23 +45,24 @@ model3 = model3.to(device)
 
 
 with torch.no_grad():
-    img, _ = data.testloader.dataset[10]
-    img_linear = img.view(-1).unsqueeze(0).to(device)
+    for i in range(5):
+        img, _ = data.testloader.dataset[i]
+        img_linear = img.view(-1).unsqueeze(0).to(device)
 
-    model0_output = model0(img_linear)
-    model1_output = model1(model0_output)
-    model2_output = model2(model1_output)
-    output = model3(model2_output)
+        model0_output = model0(img_linear)
+        model1_output = model1(model0_output)
+        model2_output = model2(model1_output)
+        output = model3(model2_output)
 
-    # Visualize original and reconstructed image
-    fig, axs = plt.subplots(1, 2)
-    axs[0].imshow(img.squeeze(), cmap='gray')
-    axs[0].set_title('Original')
+        # Visualize original and reconstructed image
+        fig, axs = plt.subplots(1, 2)
+        axs[0].imshow(img.squeeze(), cmap='gray')
+        axs[0].set_title('Original')
 
-    axs[1].imshow(output[0].view(28, 28).cpu(), cmap='gray')
-    axs[1].set_title('Reconstructed')
-    for ax in axs: ax.axis('off')
-    plt.show()
+        axs[1].imshow(output[0].view(28, 28).cpu(), cmap='gray')
+        axs[1].set_title('Reconstructed')
+        for ax in axs: ax.axis('off')
+        plt.show()
 
 plt.figure(figsize=(8, 4))
 plt.subplot(1, 2, 1)
