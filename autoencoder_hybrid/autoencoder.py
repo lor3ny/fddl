a92031194e5a28c204f733c34aebfe5fca66cc4d
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2025-07-02 11:18:20 +0200

    Manual backward to maintain context

--- autoencoder_hybrid/autoencoder.py
+++ autoencoder_hybrid/autoencoder.py
@@ -3,7 +3,75 @@ import torch.nn as nn
 import torch.nn.functional as F
 from mpi4py import MPI
 
+class LinearMPI(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx,
+            async_grad_allreduce,
+            rank, gpu_rank, size, comm, A=None, B=None, bias=None):
+        
+        ctx.save_for_backward(A, B)
+        ctx.use_bias = bias is not None
+        ctx.async_grad_allreduce = async_grad_allreduce
 
+        N = A.size(dim=0)
+        K = B.size(dim=0)
+        M = B.size(dim=1)
+        
+        local_rows = N // size
+
+        # Scatter A over the ranks
+        A_local = torch.empty(local_rows, K, dtype=torch.float32)
+        comm.Scatter([A.detach().cpu(), MPI.FLOAT], [A_local, MPI.FLOAT], root=0)
+        # Give weights B to every process
+        comm.Bcast([B.detach().cpu(), MPI.FLOAT], root=0)
+
+        # Bring everything to the GPU
+        A_local = A_local.to(gpu_rank)
+        B = B.to(gpu_rank)
+
+        # Actual matmul
+        C_local = torch.matmul(A_local, B)
+
+        if rank == 0:
+            C_local_cpu = C_local.detach().cpu()
+            C = torch.empty(N, M, dtype=torch.float32)
+            comm.Gather([C_local_cpu, MPI.FLOAT], [C, MPI.FLOAT], root=0)
+            C.grad = A.grad
+            return C.to(gpu_rank)
+        else:
+            C_local_cpu = C_local.detach().cpu()
+            comm.Gather([C_local_cpu, MPI.FLOAT], None, root=0)
+            return torch.zeros(N, M, device=f"cuda:{gpu_rank}") 
+
+    @staticmethod
+    def backward(ctx, 
+            grad_output):
+        input, weight = ctx.saved_tensors
+        use_bias = ctx.use_bias
+        grad_input = grad_output.matmul(weight)
+        grad_output = grad_output.contiguous()
+
+        '''
+        if ctx.async_grad_allreduce:
+            # Asynchronous all-reduce
+            """
+            From PyTorch docs: this reduces the tensor data across all machines in
+            a way that all get the final reduced result. Default reduction op
+            is SUM (torch.distributed.ReduceOp.SUM) specified by the parameter
+            'op'.
+            """
+            handle = torch.distributed.all_reduce(
+                grad_input, group=get_tensor_model_parallel_group(), async_op=True
+            )
+        '''    
+        grad_weight = grad_output.t().matmul(input)
+        grad_bias = grad_output.sum(dim=0) if use_bias else None
+
+        '''
+        if ctx.async_grad_allreduce:
+            handle.wait()
+        '''
+        return grad_input, grad_weight, grad_bias, None, None, None
 
 
 class DistributedOperations():
@@ -66,10 +134,8 @@ class ManualLinear(nn.Module):
         # bias: (out_features)
         #print(f"RANK {self.rank} GPU {self.gpu_rank} Started MATMUL!", flush=True)
         if True:
-            if self.rank == 0:
-                return DistributedOperations.DistributedMatmul(self.rank, self.gpu_rank, self.size, self.comm, A=x, B=self.weight.t()) + self.bias #torch.matmul(x, self.weight.t()) + self.bias
-            else:
-                return DistributedOperations.DistributedMatmul(self.rank, self.gpu_rank, self.size, self.comm, A=x, B=self.weight.t())
+            out = LinearMPI.apply(False, self.rank, self.gpu_rank, self.size, self.comm, A=x, B=self.weight.t(), self.bias)
+            return out
         else:
             return torch.matmul(x, self.weight.t()) + self.bias
 
